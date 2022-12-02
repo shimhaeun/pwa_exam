5fb9c508e60cfaa24fe6d2d8b7cca4d7fa5cf90e
haeun <shy624@naver.com>
2022-12-02 17:47:55 +0900

    서비스 워커 등록, index.html 추가

--- service_worker.js
+++ service_worker.js
@@ -20,3 +20,17 @@ self.addEventListener("install", pEvent => {
 self.addEventListener('active', pEvent => {
     console.log('서비스워커 동작 시작됨!');
 });
+// 고유 번호를 할당 받은 서비스워커 작동
+self.addEventListener('fetch', pEvent => {
+    pEvent.respondWith(
+        caches.match(pEvent.request)
+        .then(response => {
+            if(!response){
+                console.log("네트워크로 데이터 요청!", pEvent.request)
+                return fetch(pEvent.request)
+            }
+            console.log("캐시에서 데이터 요청!",pEvent.request)
+            return response;
+        }).catch(err => console.log(err))
+    );
+});
