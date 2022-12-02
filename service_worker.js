b932e63e5e2f381e59fd9ea39b4ac67fb6dfd33b
haeun <shy624@naver.com>
2022-12-02 17:06:11 +0900

    service worker 내용 추가

--- service_worker.js
+++ service_worker.js
@@ -5,3 +5,14 @@ const aFilesToCache = [ //캐시할 파일 지정
     './manifest.json',
     './images/hello-pwa.png'
 ];
+// 서비스워커 실행 & 캐시파일 저장
+self.addEventListener("install", pEvent => {
+    console.log("서비스 워커 설치 완료!");
+    pEvent.waitUntil(
+        caches.open(sCacheName)
+        .then(pCache => {
+            console.log("캐시에 파일 저장 완료!");
+            return pCache.addAll(aFilesToCache);
+        })
+    );
+});
